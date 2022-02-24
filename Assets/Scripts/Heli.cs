bd6f31ebd417976060a28e697c4dadb1b745975e
Hendrik <hein@grobler.dev>
2022-02-24 00:50:06 +0000

    Progress on collision

--- Assets/Scripts/Heli.cs
+++ Assets/Scripts/Heli.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class Heli : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int sol;
     void Start()
     {
-        
+        sol = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Movement for the heli
         transform.Translate(0,Input.GetAxis("Vertical") * 10.0f * Time.deltaTime,0);
         transform.Translate(Input.GetAxis("Horizontal") * 10.0f * Time.deltaTime,0,0);
+    }
+
+    //Deals with picking up the soldiers
+    private void OnTriggerEnter(Collider other)
+    {
+        if (true)
+        {
+            Destroy(other.gameObject);
+            sol++;
+            Debug.Log("you are carrying "+sol+" soldiers");
+        }
     }
 }
