7b1092d17d110ed8e869cabd1be6eada859ded7f
hein <hein@grobler.dev>
2022-03-02 13:37:34 +1100

    Soldier Pickup and drop off implemented

--- Assets/Scripts/Heli.cs
+++ Assets/Scripts/Heli.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Heli : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int sol;
+    private int sol;
+    private int hosp;
+    public Text solText;
     void Start()
     {
         sol = 0;
-
+        hosp = 0;
     }
 
     // Update is called once per frame
@@ -21,13 +24,28 @@ public class Heli : MonoBehaviour
     }
 
     //Deals with picking up the soldiers
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (true)
+        if (other.CompareTag("person") && sol < 3)
         {
             Destroy(other.gameObject);
             sol++;
+            solText.text = sol.ToString();
             Debug.Log("you are carrying "+sol+" soldiers");
+        }
+        
+        if (other.CompareTag("hospital"))
+        {
+            hosp = hosp + sol;
+            sol = 0;
+            solText.text = sol.ToString();
+            Debug.Log(hosp+" soldiers in hospital");
+            Debug.Log("you are carrying "+sol+" soldiers");
+        }
+        
+        if (other.CompareTag("tree"))
+        {
+            Debug.Log("tree hit, game over");
         }
     }
 }
